c7960d29835e37cd610ee492bbf2145f5bd9be77
ujjwal <ujjwal.singh@subtlelabs.com>
2025-11-05 17:33:30 +0530

    new comment added

--- projectx_backend/urls.py
+++ projectx_backend/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
     path('shopify/', include('shopify_integration.urls')),
+
+    #lightspeed endpoints
     path("lightspeed/", include("lightspeed_integration.urls")),
+
 
 ]
